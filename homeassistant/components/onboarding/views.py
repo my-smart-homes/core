f17a5010083ae696b8e660994ed59455882c1965
Naimur <naimurhasanrwd@gmail.com>
2024-11-29 12:32:11 +0600

    fix: account create translation stuck

--- homeassistant/components/onboarding/views.py
+++ homeassistant/components/onboarding/views.py
@@ -194,7 +194,9 @@ class UserOnboardingView(_BaseOnboardingView):
             area_registry = ar.async_get(hass)
 
             for area in DEFAULT_AREAS:
-                name = translations[f"component.onboarding.area.{area}"]
+                name = translations.get(
+                    f"component.onboarding.area.{area}", "Living Room"
+                )
                 # Guard because area might have been created by an automatically
                 # set up integration.
                 if not area_registry.async_get_area_by_name(name):
